f46185477be2ab426bcd2610ba5f34958e7b0091
Jonathan Hall <flimzy@flimzy.com>
2024-07-08 17:04:07 +0200

    Trim a bit more fat

--- main.go
+++ main.go
@@ -5,19 +5,13 @@ import (
 	"database/sql"
 	"database/sql/driver"
 
-	_ "modernc.org/sqlite"
+	"modernc.org/sqlite"
 )
 
 func main() {
-	db, err := sql.Open("sqlite", ":memory:")
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-	drv := db.Driver()
-	edrv := New(drv)
+	edrv := New(&sqlite.Driver{})
 	sql.Register("proxy", edrv)
-	db, err = sql.Open("proxy", ":memory:")
+	db, err := sql.Open("proxy", ":memory:")
 	if err != nil {
 		panic(err)
 	}
@@ -38,8 +32,6 @@ type Driver struct {
 	driver.Driver
 }
 
-var _ driver.Driver = (*Driver)(nil)
-
 // New returns a new Driver instance, which wraps driver and calls eh for
 // any errors. The default error handler just passes the error through,
 // unaltered.
@@ -59,8 +51,6 @@ type connWrapper struct {
 	driver.Conn
 }
 
-var _ driver.Conn = (*connWrapper)(nil)
-
 func (c *connWrapper) Prepare(query string) (driver.Stmt, error) {
 	stmt, err := c.Conn.Prepare(query)
 	if err != nil {
@@ -73,25 +63,10 @@ type stmtWrapper struct {
 	driver.Stmt
 }
 
-var (
-	_ driver.Stmt            = (*stmtWrapper)(nil)
-	_ driver.ColumnConverter = (*stmtWrapper)(nil)
-)
-
 func (s *stmtWrapper) ColumnConverter(idx int) driver.ValueConverter {
 	columnConverter, ok := s.Stmt.(driver.ColumnConverter)
 	if !ok {
 		return driver.DefaultParameterConverter
 	}
-	return driver.ValueConverter(&valueConverterWrapper{v: columnConverter.ColumnConverter(idx)})
-}
-
-type valueConverterWrapper struct {
-	v driver.ValueConverter
-}
-
-var _ driver.ValueConverter = valueConverterWrapper{}
-
-func (vc valueConverterWrapper) ConvertValue(v any) (driver.Value, error) {
-	return vc.v.ConvertValue(v)
+	return columnConverter.ColumnConverter(idx)
 }
