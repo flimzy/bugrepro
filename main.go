e66a6da960815d283c7fcf1a6b2e58cdfe0d946a
Jonathan Hall <flimzy@flimzy.com>
2024-07-08 17:02:04 +0200

    Merge everything into main.go

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"context"
 	"database/sql"
+	"database/sql/driver"
 
-	errsql "github.com/flimzy/bugrepro/proxy"
 	_ "modernc.org/sqlite"
 )
 
@@ -15,7 +15,7 @@ func main() {
 	}
 	defer db.Close()
 	drv := db.Driver()
-	edrv := errsql.New(drv)
+	edrv := New(drv)
 	sql.Register("proxy", edrv)
 	db, err = sql.Open("proxy", ":memory:")
 	if err != nil {
@@ -31,4 +31,67 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+// Driver is the core type implementing the [database/sql/driver.Driver] interface.
+type Driver struct {
+	driver.Driver
+}
+
+var _ driver.Driver = (*Driver)(nil)
+
+// New returns a new Driver instance, which wraps driver and calls eh for
+// any errors. The default error handler just passes the error through,
+// unaltered.
+func New(driver driver.Driver) *Driver {
+	return &Driver{Driver: driver}
+}
+
+func (d *Driver) Open(name string) (driver.Conn, error) {
+	conn, err := d.Driver.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	return driver.Conn(&connWrapper{Conn: conn}), nil
+}
+
+type connWrapper struct {
+	driver.Conn
+}
+
+var _ driver.Conn = (*connWrapper)(nil)
+
+func (c *connWrapper) Prepare(query string) (driver.Stmt, error) {
+	stmt, err := c.Conn.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+	return driver.Stmt(&stmtWrapper{Stmt: stmt}), nil
+}
+
+type stmtWrapper struct {
+	driver.Stmt
+}
+
+var (
+	_ driver.Stmt            = (*stmtWrapper)(nil)
+	_ driver.ColumnConverter = (*stmtWrapper)(nil)
+)
+
+func (s *stmtWrapper) ColumnConverter(idx int) driver.ValueConverter {
+	columnConverter, ok := s.Stmt.(driver.ColumnConverter)
+	if !ok {
+		return driver.DefaultParameterConverter
+	}
+	return driver.ValueConverter(&valueConverterWrapper{v: columnConverter.ColumnConverter(idx)})
+}
+
+type valueConverterWrapper struct {
+	v driver.ValueConverter
+}
+
+var _ driver.ValueConverter = valueConverterWrapper{}
+
+func (vc valueConverterWrapper) ConvertValue(v any) (driver.Value, error) {
+	return vc.v.ConvertValue(v)
 }
